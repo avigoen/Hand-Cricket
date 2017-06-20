694e82d45ade85e3629f646bfe94bde787c9273c
Avinash Goen <agoen97@gmail.com>
2017-06-20 15:48:37 +0530

    Instructions for in-game use

---
+++ info.java
@@ -0,0 +1,15 @@
+package PulzionGame;
+public class info
+{
+    public static int userBatFirst, compBatFirst, userTotalRuns, compTotalRuns;
+    public int ballsRemaining;
+    public info(int a, int b)
+    {
+        userBatFirst=a;
+        compBatFirst=b;
+    }
+
+    public info()
+    {
+    }
+}
